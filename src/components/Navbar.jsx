37a131c87d44f6492116ad91d294d93b925c2ef7
OdilovMM <dssrinman@gmail.com>
2024-08-06 16:04:00 +0500

    Add: Dynamic Theme changing darkmode/lightmode  ✔

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,32 @@ import { BiCartAlt } from "react-icons/bi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 import Navlinks from "./Navlinks";
+import { useEffect, useState } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+
+const themes = {
+  acid: "acid",
+  dracula: "dracula",
+};
+
+const getThemeLocalStorage = () => {
+  return localStorage.getItem("theme") || theme.acid;
+};
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeLocalStorage);
+
+  const changeTheme = () => {
+    const { acid, dracula } = themes;
+    const newTheme = theme === acid ? dracula : acid;
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className=" bg-base-200">
       <div className="navbar make-center">
@@ -35,9 +59,14 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={changeTheme} className="hidden" />
+            <BsSunFill className="swap-on h-4 w-4" />
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
           <NavLink to="/" className="btn btn-ghost btn-circle btn-md ml-4">
             <div className="indicator">
-              <IoIosHeartEmpty className="h-6 w-6" color="blue" />
+              <IoIosHeartEmpty className="h-6 w-6" color="white" />
               <span className="badge badge-sm badge-primary indicator-item">
                 2
               </span>
@@ -45,7 +74,7 @@ const Navbar = () => {
           </NavLink>
           <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
             <div className="indicator">
-              <BiCartAlt className="h-6 w-6" color="blue" />
+              <BiCartAlt className="h-6 w-6" color="white" />
               <span className="badge badge-sm badge-primary indicator-item">
                 5
               </span>
