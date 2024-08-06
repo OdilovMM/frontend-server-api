f6ec8f982c4aafbdf6e94a1b53c2751f85531c4f
OdilovMM <dssrinman@gmail.com>
2024-08-06 12:50:33 +0500

    Add: Routers setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
   About,
   Cart,
@@ -8,11 +9,60 @@ import {
   Login,
   Orders,
   Products,
+  Register,
   SingleProductPage,
 } from "./pages";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:productId",
+        element: <SingleProductPage />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
