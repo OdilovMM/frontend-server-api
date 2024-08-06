83c26a0d3dd085ce5bdb2bba55a258ecc9f9262c
OdilovMM <dssrinman@gmail.com>
2024-08-06 12:40:36 +0500

    Add: Pages creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  ErrorPage,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  SingleProductPage,
+} from "./pages";
 
 const App = () => {
   return <div>App</div>;
---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import About from "./About";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import ErrorPage from "./ErrorPage";
+import HomeLayout from "./HomeLayout";
+import Landing from "./Landing";
+import Login from "./Login";
+import Orders from "./Orders";
+import Products from "./Products";
+import Register from "./Register";
+import SingleProductPage from "./SingleProductPage";
+
+export {
+  About,
+  Cart,
+  Checkout,
+  ErrorPage,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProductPage,
+};
